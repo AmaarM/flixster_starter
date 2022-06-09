da979ed7a4ba0a23fb3c7b44cf53ef8502516545
Amaar Mohamed <codebyamaar@gmail.com>
2022-06-09 17:47:38 -0500

    added features

--- script.js
+++ script.js
@@ -1,34 +1,66 @@
 const apiKey = '3924b5d38a0a34d058ffe49928a2a842';
+const submitBtn = document.getElementById("submit");
+const textBox = document.getElementById("inputbox");
+const form = document.getElementById("form");
+const getMore = document.querySelector(".getMore")
+const footer = document.querySelector(".footer");
+let page = 1;
 let movieArea = document.querySelector(".movie-wrapper");
-const submitBtn = document.querySelector("#submit");
-const textBox = document.querySelector("#input-box");
-const form = document.querySelector("#form");
-let page = 10;
 
 
-
+console.log(textBox.value);
 async function getMovies(e){
     e.preventDefault();
     let url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&page=${page}`;
-
     const data = await fetch(url);
     const getData = await data.json();
-    console.log(getData)
-    console.log(getData.results[2]);
-    page+=10;
 
+    page+=1;
+    console.log(getData);
     getData.results.forEach(e =>{
         displayMovies(e);
     });
 }
 
-function displayMovies(data){
-    movieArea += `<img src="data.poster_path">`;
+async function displayMovies(data){
+    let moviePath = `https://image.tmdb.org/t/p/w185` + data.poster_path;
+    let movieTitle = data.title;
+    let movieRating = data.vote_average;
+
+    movieArea.innerHTML += ` 
+    <div class="innerMovieArea">     
+        <img class="oneMovie" src='${moviePath}'>    
+        <h3 class="rating">Rating: ${movieRating} </h3>
+        <h3 class="movie-title">${movieTitle}</h3>
+    </div>`
+    footer.classList.remove('closed')
 }
 
 
-form.addEventListener('submit', handleFormSubmits);
-function handleFormSubmits(e){
+getMore.addEventListener('click', getMoreResults);
+function getMoreResults(e){
+    e.preventDefault();
     getMovies(e);
+}
 
+submitBtn.addEventListener('click', searchMovie);
+async function searchMovie(e){
+    e.preventDefault();
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=` + textBox.value + `&page=1&include_adult=false`;
+    const data = await fetch(url);
+    const searchData = await data.json();
+    console.log(searchData);
+    searchData.results.forEach(e =>{
+        displayMovies(e);
+    });
+
+}
+
+textBox.addEventListener(e, async e =>{
+    
+})
+
+window.onload = (e) => {
+    e.preventDefault();
+    getMovies(e);
 }
