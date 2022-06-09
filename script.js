fd9a2ebad355f3e5b1bf04bee8516197ba67c76e
Amaar Mohamed <amaarmoh@Amaars-MacBook-Air.local>
2022-06-09 12:21:42 -0500

    update

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 const apiKey = '3924b5d38a0a34d058ffe49928a2a842';
-const movieArea = document.querySelector(".movie-wrapper");
+let movieArea = document.querySelector(".movie-wrapper");
 const submitBtn = document.querySelector("#submit");
 const textBox = document.querySelector("#input-box");
 const form = document.querySelector("#form");
-let page = 0;
+let page = 10;
 
 
 
 async function getMovies(e){
     e.preventDefault();
-    let url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`
+    let url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&page=${page}`;
 
     const data = await fetch(url);
     const getData = await data.json();
-    console.log(getData);
+    console.log(getData)
+    console.log(getData.results[2]);
+    page+=10;
+
+    getData.results.forEach(e =>{
+        displayMovies(e);
+    });
+}
+
+function displayMovies(data){
+    movieArea += `<img src="data.poster_path">`;
 }
 
 
